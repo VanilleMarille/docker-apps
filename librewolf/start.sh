18b95ce56bf5ded827e2d067371f7eb740efb78c
Baumgartner <personal@j-baum.at>
2021-11-20 14:36:47 +0100

    Added librewolf browser

---
+++ librewolf/start.sh
@@ -0,0 +1,13 @@
+docker run -it --rm \
+    --device /dev/snd \
+    -e PULSE_SERVER=unix:${XDG_RUNTIME_DIR}/pulse/native \
+    -e DISPLAY=$DISPLAY \
+    --net=host \
+    -v $HOME/.dockerapps/librewolf:/home/user/ \
+    -v $HOME/Downloads:/home/user/Downloads \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v $HOME/.Xauthority:/root/.Xauthority:rw \
+    -v ${XDG_RUNTIME_DIR}/pulse/native:${XDG_RUNTIME_DIR}/pulse/native \
+    -v ~/.config/pulse/cookie:/root/.config/pulse/cookie \
+    --group-add $(getent group audio | cut -d: -f3) \
+    librewolf
