aa8950176fc3b994a0d9f4955c22296b7d38d90d
user4022 <personal@j-baum.at>
2021-12-24 23:02:24 +0100

    mount selenium.d

--- firefox-python-selenium/start.sh
+++ firefox-python-selenium/start.sh
@@ -1,10 +1,13 @@
 xhost +
 
+MYPATH=(pwd)
+
 docker run -it --rm \
     --device /dev/snd \
     -e PULSE_SERVER=unix:${XDG_RUNTIME_DIR}/pulse/native \
     -e DISPLAY=$DISPLAY \
     --net=host \
+    -v $MYPATH/selenium.d/:/home/user/selenium.d \
     -v $HOME/.dockerapps/firefox-python-selenium:/home/user \
     -v $HOME/Downloads:/home/user/Downloads \
     -v ${XDG_RUNTIME_DIR}/pulse/native:${XDG_RUNTIME_DIR}/pulse/native \
