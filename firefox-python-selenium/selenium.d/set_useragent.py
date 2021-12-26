99c2278482e2a123342dff4a50b77503841c57b7
user4022 <personal@j-baum.at>
2021-12-26 18:50:45 +0100

    added windows 10 useragent

--- firefox-python-selenium/selenium.d/set_useragent.py
+++ firefox-python-selenium/selenium.d/set_useragent.py
@@ -3,5 +3,5 @@
 from selenium import webdriver
 
 profile=webdriver.FirefoxProfile()
-profile.set_preference("general.useragent.override", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:63.0) Gecko/20100101 Firefox/63.0")
+profile.set_preference("general.useragent.override", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:80.0) Gecko/20100101 Firefox/80.0")
 driver=webdriver.Firefox(profile)
