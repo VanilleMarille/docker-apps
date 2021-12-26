d615fd4c5a0c92c0825753b731e3e56295c3fb0e
user4022 <personal@j-baum.at>
2021-12-26 18:53:20 +0100

    added any key

--- firefox-python-selenium/selenium.d/set_useragent.py
+++ firefox-python-selenium/selenium.d/set_useragent.py
@@ -5,3 +5,5 @@ from selenium import webdriver
 profile=webdriver.FirefoxProfile()
 profile.set_preference("general.useragent.override", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:80.0) Gecko/20100101 Firefox/80.0")
 driver=webdriver.Firefox(profile)
+
+input("Press any key to close")
