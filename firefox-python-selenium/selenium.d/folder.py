e10489edd5a0f74f9be1cfd01c135cc1618fd3f0
user4022 <personal@j-baum.at>
2021-12-29 18:35:28 +0100

    Selenium with permanent profile

--- firefox-python-selenium/selenium.d/folder.py
+++ firefox-python-selenium/selenium.d/folder.py
@@ -1,17 +1,47 @@
 #!/usr/bin/env python
 
+import os
 import glob
+import subprocess
 from selenium import webdriver
 
+#Select newest profile#
+saved_profiles=(os.environ['HOME']) + '/.dockerapps/firefox-python-selenium/profiles'
+
+profiles=os.listdir(saved_profiles)
+
+profile_count=0
+for profile in profiles:
+	profile_count += 1
 
 
+profile=(saved_profiles + "/profile_" + str(profile_count))
+print("Selecting Profile: " + profile)
+exit()
+
+#Configure selenium#
+profile.set_preference("dom.webdriver.enabled", False)
+profile.set_preference('useAutomationExtension', False)
+profile.set_preference("network.cookie.cookieBehavior", 1)
+profile.update_preferences()
+desired = DesiredCapabilities.FIREFOX
+
+options=webdriver.FirefoxOptions()
+
+#Start selenium#
 profiles_old=set(glob.glob('/tmp/rust_mozprofile*'))
 
-browser=webdriver.Firefox()
+browser=webdriver.Firefox(options=options, firefox_profile=profile, desired_capabilities=desired) 
 
 profiles_new=set(glob.glob('/tmp/rust_mozprofile*'))
 
 current_profile=(profiles_old.symmetric_difference(profiles_new))
-print(current_profile)
+for convert in current_profile:
+	current_profile=convert
+
+#Save new profile and quit selenium#
+input("Press any key to save new profile")
+
+subprocess.run(["cp", "-r", current_profile, "/home/user"]) 
 
 browser.quit()
